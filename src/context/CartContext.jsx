b81fd3fce0f61ebd0a3acb446a0040806e565c09
Artem389 <a.basinov10@gmail.com>
2025-11-12 18:23:08 +0300

    3

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -198,7 +198,7 @@ export const CartProvider = ({ children }) => {
     return cartState.items.some(item => item.name === itemName);
   };
 
-  // Значение контекста (сохранена обратная совместимость)
+  // Значение контекста 
   const cartContextValue = {
     cart: {
       items: cartState.items
@@ -228,85 +228,3 @@ export const useCart = () => {
 };
 
 
-
-
-// import React, { createContext, useContext, useReducer } from 'react';
-
-// const CartContext = createContext();
-
-// const cartReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'ADD_ITEM':
-//       const existingItem = state.items.find(item => item.name === action.payload.name);
-//       if (existingItem) {
-//         return {
-//           ...state,
-//           items: state.items.map(item =>
-//             item.name === action.payload.name
-//               ? { ...item, quantity: item.quantity + 1 }
-//               : item
-//           )
-//         };
-//       }
-//       return {
-//         ...state,
-//         items: [...state.items, { ...action.payload, quantity: 1 }]
-//       };
-//     case 'REMOVE_ONE_ITEM':
-//       const itemToRemove = state.items.find(item => item.name === action.payload.name);
-//       if (itemToRemove && itemToRemove.quantity > 1) {
-//         return {
-//           ...state,
-//           items: state.items.map(item =>
-//             item.name === action.payload.name
-//               ? { ...item, quantity: item.quantity - 1 }
-//               : item
-//           )
-//         };
-//       } else {
-//         return {
-//           ...state,
-//           items: state.items.filter(item => item.name !== action.payload.name)
-//         };
-//       }
-//     case 'REMOVE_ITEM':
-//       return {
-//         ...state,
-//         items: state.items.filter(item => item.name !== action.payload.name)
-//       };
-//     case 'CLEAR_CART':
-//       return { items: [] };
-//     default:
-//       return state;
-//   }
-// };
-
-// export const CartProvider = ({ children }) => {
-//   const [cart, dispatch] = useReducer(cartReducer, { items: [] });
-
-//   const addItem = (item) => {
-//     dispatch({ type: 'ADD_ITEM', payload: item });
-//   };
-
-//   const removeOneItem = (item) => {
-//     dispatch({ type: 'REMOVE_ONE_ITEM', payload: item });
-//   };
-
-//   const removeItem = (item) => {
-//     dispatch({ type: 'REMOVE_ITEM', payload: item });
-//   };
-
-//   const getTotalPrice = () => {
-//     return cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
-//   };
-
-//   return (
-//     <CartContext.Provider value={{ cart, addItem, removeOneItem, removeItem, getTotalPrice }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
-
-// export const useCart = () => {
-//   return useContext(CartContext);
-// };
